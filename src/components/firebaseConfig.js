86d69cde434197e263bc422840583eac1bebd0e7
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-09-22 17:00:27 -0300

    firebaseConfig commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,14 @@
+//Exportação das configurações do banco de dados
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBLAP5Da11Dhfzd4KbuCWPYrf41MdJsSzo",
+    authDomain: "digitalc-3e567.firebaseapp.com",
+    projectId: "digitalc-3e567",
+    storageBucket: "digitalc-3e567.appspot.com",
+    messagingSenderId: "694261131096",
+    appId: "1:694261131096:web:7c91464d49e5707e01e72d",
+    measurementId: "G-09KG4FV9TB"
+  };
+  
+  export default firebaseConfig;
+  
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,16 +4,7 @@ import google from './images/google.png';
 import apple from './images/apple.png';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBLAP5Da11Dhfzd4KbuCWPYrf41MdJsSzo",
-  authDomain: "digitalc-3e567.firebaseapp.com",
-  projectId: "digitalc-3e567",
-  storageBucket: "digitalc-3e567.appspot.com",
-  messagingSenderId: "694261131096",
-  appId: "1:694261131096:web:7c91464d49e5707e01e72d",
-  measurementId: "G-09KG4FV9TB"
-};
+import firebaseConfig from './firebaseConfig.js'
 
 const app = initializeApp(firebaseConfig);
 
